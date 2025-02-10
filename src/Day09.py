99c99a3af1618c0a942144b5ccba52166245324d
Timothée Boulet <timboulet@hotmail.fr>
2025-02-11 00:39:11 +0100

    Day 09 Star 2

--- src/Day09.py
+++ src/Day09.py
@@ -6,18 +6,44 @@ if __name__ == '__main__':
         heightmap = np.array([list(map(int, line)) for line in f.read().split('\n')])
 
     risk_level = 0
+    bassins_idx = {}
+    idx_bassins = {}
     for (x, y), h in np.ndenumerate(heightmap):
-        high_neighbors = 0
-        if x > 0 and heightmap[x-1, y] <= h:
+        if h == 9:
             continue
-        if x < heightmap.shape[0] - 1 and heightmap[x+1, y] <= h:
-            continue
-        if y > 0 and heightmap[x, y-1] <= h:
-            continue
-        if y < heightmap.shape[1] - 1 and heightmap[x, y+1] <= h:
-            continue
-        risk_level += h+1
+
+        is_low_point = True
+        neigbors_bassins = []
+        if x > 0:
+            is_low_point = min(is_low_point, heightmap[x-1, y] > h)
+            if (x-1, y) in idx_bassins:
+                neigbors_bassins.append(idx_bassins[(x-1, y)])
+        if x < heightmap.shape[0] - 1:
+            is_low_point = min(is_low_point, heightmap[x+1, y] > h)
+            if (x+1, y) in idx_bassins:
+                neigbors_bassins.append(idx_bassins[(x+1, y)])
+        if y > 0:
+            is_low_point = min(is_low_point, heightmap[x, y-1] > h)
+            if (x, y-1) in idx_bassins:
+                neigbors_bassins.append(idx_bassins[(x, y-1)])
+        if y < heightmap.shape[1] - 1 :
+            is_low_point = min(is_low_point, heightmap[x, y+1] > h)
+            if (x, y+1) in idx_bassins:
+                neigbors_bassins.append(idx_bassins[(x, y+1)])
+
+        if is_low_point:
+            risk_level += h+1
+
+        final_bassin = min(neigbors_bassins, default=max(bassins_idx, default=0) + 1)
+        idx_bassins[(x, y)] = final_bassin
+        bassins_idx[final_bassin] = bassins_idx.get(final_bassin, []) + [(x, y)]
+        for b in neigbors_bassins:
+            if b != final_bassin:
+                for i, j in bassins_idx[b]:
+                    idx_bassins[(i, j)] = final_bassin
+                    bassins_idx[final_bassin].append((i, j))
+                del bassins_idx[b]
 
     print(f"The result of first star is {risk_level}")
 
-    print(f"The result of second star is {0}")
+    print(f"The result of second star is {np.prod(sorted([len(bassins_idx[b]) for b in bassins_idx])[-3:])}")
